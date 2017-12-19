c9854636c9c0cd56d4773b3e4404a82cc1b0553f
weektrip <weektrip@foxmail.com>
2017-12-19 17:26:41 +0800

    cms系统更新

--- application/extra/addons.php
+++ application/extra/addons.php
@@ -11,5 +11,11 @@ return array (
   ),
   'route' => 
   array (
+    '/$' => 'cms/index/index',
+    '/c/[:diyname]' => 'cms/channel/index',
+    '/t/[:name]' => 'cms/tags/index',
+    '/a/[:diyname]' => 'cms/archives/index',
+    '/p/[:diyname]' => 'cms/page/index',
+    '/s' => 'cms/search/index',
   ),
 );
